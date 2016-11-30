d42b81e6729f5a5f7967736b6bdc66a5f078c227
naganowl <alan@mavenlink.com>
2016-11-30 10:15:07 -0800

    Wrap index.js spec in describe block

--- test/index.js
+++ test/index.js
@@ -27,44 +27,47 @@ const transform = (resourcePath) => {
   })(fileContent);
 };
 
-it('parses test/kitno-loader.coffee', () => {
-  const kitnoLoaderSourcePath = 'test/example/kitno-loader.source.coffee';
-  const kitnoLoaderOutput = fs.readFileSync('test/example/kitno-loader.output.coffee');
-  expect(transform(kitnoLoaderSourcePath).toString()).toEqual(kitnoLoaderOutput.toString());
-});
+describe("kitno-loader", () => {
+  it('parses test/kitno-loader.coffee', () => {
+    const kitnoLoaderSourcePath = 'test/example/kitno-loader.source.coffee';
+    const kitnoLoaderOutput = fs.readFileSync('test/example/kitno-loader.output.coffee');
+    expect(transform(kitnoLoaderSourcePath).toString()).toEqual(kitnoLoaderOutput.toString());
+  });
 
-it('parses test/kitno-loader.coffee', () => {
-  const loaderSourcePath = 'test/example/loader.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/loader.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
-});
+  it('parses test/kitno-loader.coffee', () => {
+    const loaderSourcePath = 'test/example/loader.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/loader.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
 
-it('parses test/multi-namespace.coffee', () => {
-  const loaderSourcePath = 'test/example/multi-namespace.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/multi-namespace.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
-});
+  it('parses test/multi-namespace.coffee', () => {
+    const loaderSourcePath = 'test/example/multi-namespace.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/multi-namespace.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
 
-it('parses test/namespace-collision.coffee', () => {
-  const loaderSourcePath = 'test/example/namespace-collision.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/namespace-collision.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
-});
+  it('parses test/namespace-collision.coffee', () => {
+    const loaderSourcePath = 'test/example/namespace-collision.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/namespace-collision.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
 
-it('parses test/duplicate-names.coffee', () => {
-  const loaderSourcePath = 'test/example/duplicate-names.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/duplicate-names.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
-});
+  it('parses test/duplicate-names.coffee', () => {
+    const loaderSourcePath = 'test/example/duplicate-names.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/duplicate-names.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
 
-it('parses test/double-duplicate-names.coffee', () => {
-  const loaderSourcePath = 'test/example/double-duplicate-names.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/double-duplicate-names.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
-});
+  it('parses test/double-duplicate-names.coffee', () => {
+    const loaderSourcePath = 'test/example/double-duplicate-names.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/double-duplicate-names.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
 
-it('parses test/double-duplicate-internal-names.coffee', () => {
-  const loaderSourcePath = 'test/example/double-duplicate-internal-names.source.coffee';
-  const loaderOutput = fs.readFileSync('test/example/double-duplicate-internal-names.output.coffee');
-  expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  it('parses test/double-duplicate-internal-names.coffee', () => {
+    const loaderSourcePath = 'test/example/double-duplicate-internal-names.source.coffee';
+    const loaderOutput = fs.readFileSync('test/example/double-duplicate-internal-names.output.coffee');
+    expect(transform(loaderSourcePath).toString()).toEqual(loaderOutput.toString());
+  });
+
 });
